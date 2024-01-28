0dfc7516baa77d5d768312dde8f583cee3ee7f8e
gxz_pc <xizhuogao@foxmail.com>
2024-01-29 00:52:23 +0800

    数据库认证

--- src/main/java/com/gxz/config/SecurityConfig.java
+++ src/main/java/com/gxz/config/SecurityConfig.java
@@ -1,9 +1,10 @@
 package com.gxz.config;
 
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -11,8 +12,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 //在Spring Security 5.4.0及更高版本中，WebSecurityConfigurerAdapter 已经被逐渐淘汰
 @Configuration
@@ -24,17 +27,27 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
                 (authz) -> authz
-                        .requestMatchers("/","/css/**","/pic/**").permitAll()  //首页和静态资源所有人都可以访问
+                        .requestMatchers("/","/index","/css/**","/pic/**").permitAll()  //首页和静态资源所有人都可以访问
                         .requestMatchers("/views/level1/**").hasRole("vip1") //level1下有vip1权限才能访问
                         .requestMatchers("/views/level2/**").hasRole("vip2") //level2有vip2权限才能访问
                         .requestMatchers("/views/level3/**").hasRole("vip3") //level3下有vip3权限才能访问
-        ).formLogin();//没有权限时跳转到登录页面
+        ).formLogin()//没有权限时跳转到登录页面
+                .and().logout()//开启注销功能
+                .logoutUrl("/logout")//访问/logout时执行注销,绑定到注销按钮
+                .logoutSuccessUrl("/");//注销成功后跳转到/index
+
+        http.logout();
         return http.build();
     }
+
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     //2.通过UserDetailsService添加用户认证的规则
     public UserDetailsService userDetailsService() {
+        //连接数据库
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
         //对密码进行加密
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         String password = encoder.encode("password");
@@ -43,7 +56,13 @@ public class SecurityConfig {
         UserDetails user2 = User.withUsername("user2").password(password).roles("vip2").build();
         UserDetails user3 = User.withUsername("user3").password(password).roles("vip3").build();
         UserDetails admin = User.withUsername("admin").password(password).roles("vip1","vip2","vip3").build();
-        return new InMemoryUserDetailsManager(user1,user2,user3,admin);
+        //添加用户并返回
+        if(!userDetailsManager.userExists("user1")) userDetailsManager.createUser(user1);
+        if(!userDetailsManager.userExists("user2")) userDetailsManager.createUser(user2);
+        if(!userDetailsManager.userExists("user3")) userDetailsManager.createUser(user3);
+        if(!userDetailsManager.userExists("admin")) userDetailsManager.createUser(admin);
+        return  userDetailsManager;
+
     }
 
 
